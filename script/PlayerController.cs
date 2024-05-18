fa9fff639b0ef21d87dbe3ef8acd25f37e9a14af
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2024-05-18 16:02:03 -0500

    camera stuff

--- script/PlayerController.cs
+++ script/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,12 +11,19 @@ public class PlayerController : MonoBehaviour
     private CharacterController _characterController;
     public InputActionAsset _inputActionAsset;
     private InputActionMap _inputActionMap_OnFoot;
+    private InputActionMap _inputActionMap_Camera;
+
+    
     private InputAction input_move;
     private InputAction input_jump;
+    private InputAction input_look;
 
     private Vector3 inputMoveDir;
     private float inputMoveDirx;
     private float inputMoveDiry;
+    private Vector3 inputCameraVector;
+    
+     
 
     private Transform _transform;
     
@@ -32,11 +40,14 @@ public class PlayerController : MonoBehaviour
     
     public Vector3 movement;// current movment of th vilocity of the player
     public Vector3 externalVilocityOfPlayer; // vilocity of player cause by gravity and other forces
+    public Vector3 rotateBody; // current vilosity  of  player's rotation
     
     // Start is called before the first frame update
     void Start()
     {
         inputMoveDir = Vector3.zero;
+        inputCameraVector = Vector3.zero;
+        rotateBody = Vector3.zero;
         
         gobalGravity = Physics.gravity;
         externalVilocityOfPlayer = gobalGravity;
@@ -45,14 +56,21 @@ public class PlayerController : MonoBehaviour
         _characterController = GetComponent<CharacterController>();
         _inputActionMap_OnFoot = _inputActionAsset.FindActionMap("onFoot");
         _inputActionMap_OnFoot.Enable();
+        _inputActionMap_Camera = _inputActionAsset.FindActionMap("Camera");
+        _inputActionMap_Camera.Enable();
+        
         input_move = _inputActionMap_OnFoot.FindAction("move");
         input_jump = _inputActionMap_OnFoot.FindAction("jump");
+        
         
         input_move.performed += context => OnInputMove(context);
         input_move.canceled += ctx => OnInputMove(ctx);
         input_jump.performed += context => OnInputJump(context);
         input_jump.canceled += ctx => OnInputJump(ctx);
         
+        input_move = _inputActionMap_OnFoot.FindAction("look");
+        input_move.performed += context => OnInputMove(context);
+        input_move.canceled += ctx => OnInputMove(ctx);
         
         
 
@@ -75,6 +93,15 @@ public class PlayerController : MonoBehaviour
 
     }
     
+    public void OnInputLook(InputAction.CallbackContext context)
+    {
+        inputCameraVector.x = context.ReadValue<Vector2>().x;
+        inputCameraVector.z = context.ReadValue<Vector2>().y;
+        
+        Debug.Log(inputMoveDir);
+
+    }
+    
 
     // Update is called once per frame
     void Update()
@@ -82,13 +109,18 @@ public class PlayerController : MonoBehaviour
         if (!_characterController.isGrounded)
         {
             externalVilocityOfPlayer += gobalGravity*Time.deltaTime;
+            inAir = true;
             
+
         }
         else
         {
+            inAir = false;
             externalVilocityOfPlayer.y = 0;
         }
-        
+
+        rotateBody.x += inputCameraVector.x*Time.deltaTime;
+        transform.Rotate(Transform.up, rotateBody.x);
         
         _characterController.Move(
             ((
